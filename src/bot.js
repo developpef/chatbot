5d72d78a48b23e1f8ef9569e780ea7826fbc0743
Faidherbe Paul-emmanuel <paul-emmanuel.faidherbe@gfi.fr>
2017-09-22 17:08:00 +0200

    interface rest

--- src/bot.js
+++ src/bot.js
@@ -5,18 +5,18 @@ const request = require('request')
 export const bot = (body, response, callback) => {
   console.log(body)
 
-  if (body.message || body.text) {
+  if (body.message) {
     client.connect.handleMessage({ body }, response, replyMessage)
-  //} else if (body.text) {
-  //  callback(null, { result: 'Bot echo '+body.text+' :)' })
+  } else if (body.text) {
+    callback(null, { result: replyRaw(body.text) })
   } else {
     callback('No text provided')
   }
 }
 
-const replyMessage = (message, text, res) => {
+function replyRaw (text) {
   const recastaiReq = new recastai.request(process.env.REQUEST_TOKEN, process.env.LANGUAGE)
-  const content = (message ? message.content : text)
+  const content = text
 	console.log("content:"+content)
   recastaiReq.analyseText(content)
   .then(recastaiRes => {
@@ -50,18 +50,70 @@ const replyMessage = (message, text, res) => {
 							varcontent = 'Je n\'ai rien trouvé!'
 						}
 					}
-					return message ? message.reply([{ type: 'text', content: varcontent }]).then() : res.json({ result: varcontent})
+					return varcontent
 				  })
 		} else {
 			// on fait appel au moteur de conversation, pour conserver l'intelligence par defaut du bot
 			const converseReq = new recastai.request(process.env.REQUEST_TOKEN, process.env.LANGUAGE)
 			converseReq.converseText(content)
 			.then(recastaiConvRes => {
-				return message ? message.reply([{ type: 'text', content: recastaiConvRes.reply()}]).then() : res.json({ result: recastaiConvRes.reply()})
+				return recastaiConvRes.reply()
 			})
 		}
 	} else {
-		return message ? message.reply([{ type: 'text', content: varcontent }]).then() : res.json({ result: varcontent})
+		return varcontent
+	}
+  })
+}
+
+const replyMessage = (message, text, res) => {
+  const recastaiReq = new recastai.request(process.env.REQUEST_TOKEN, process.env.LANGUAGE)
+  const content = message.content
+	console.log("content:"+content)
+  recastaiReq.analyseText(content)
+  .then(recastaiRes => {
+	var varcontent = 'Je ne comprends pas...'
+	
+	// get the intent detected
+    var intent = recastaiRes.intent()
+	if(intent) {
+		console.log("intent:"+intent.slug+"/"+intent.confidence)
+		if (intent.slug === 'c8y_geoloc' && intent.confidence > 0.7) {
+			var asset = recastaiRes.get('asset-type').raw
+			console.log("asset:"+asset)
+			var number = recastaiRes.get('number').raw
+			console.log("number:"+number)
+			varcontent = 'je vais chercher la '+asset+' '+number
+			 
+			request(
+				{
+					url:'https://pefgfi.cumulocity.com/identity/externalIds/stelia_id/'+asset+'_'+number, 
+					headers : {"Authorization" : "Basic cGF1bC1lbW1hbnVlbC5mYWlkaGVyYmVAZ2ZpLmZyOiFhbjEyUEVG"}
+				},
+				(_err, _res, body) => {
+					if(_err) {
+						varcontent = 'Il y a eu un problème...'
+					} else {
+						body = JSON.parse(body)
+						console.log("C8Y resp:"+body)
+						if(body.managedObject) {
+							varcontent = 'Juste ici : '+body.managedObject['self']+' !'
+						} else {
+							varcontent = 'Je n\'ai rien trouvé!'
+						}
+					}
+					return message.reply([{ type: 'text', content: varcontent }]).then()
+				  })
+		} else {
+			// on fait appel au moteur de conversation, pour conserver l'intelligence par defaut du bot
+			const converseReq = new recastai.request(process.env.REQUEST_TOKEN, process.env.LANGUAGE)
+			converseReq.converseText(content)
+			.then(recastaiConvRes => {
+				return message.reply([{ type: 'text', content: recastaiConvRes.reply()}]).then()
+			})
+		}
+	} else {
+		return message.reply([{ type: 'text', content: varcontent }]).then()
 	}
   })
 }
